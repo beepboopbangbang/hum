b052619f09fa734873dc41e7cf65296b0a7e94a1
jojobyte <184880+jojobyte@users.noreply.github.com>
2021-10-16 14:47:37 -0600

    Add server default bind addr & port

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,14 @@ async fn main() {
     dotenv().ok();
     pretty_env_logger::init();
 
-    let addr: IpAddr = env::var("HUM_ADDR").unwrap().parse().unwrap();
-    let port: u16 = env::var("HUM_PORT").unwrap().parse().unwrap();
+    let addr: IpAddr = env::var("HUM_ADDR")
+        .unwrap_or_else(|_| "0.0.0.0".to_string())
+        .parse()
+        .unwrap();
+    let port: u16 = env::var("HUM_PORT")
+        .unwrap_or_else(|_| 3030.to_string())
+        .parse()
+        .unwrap();
 
     let routes = warp::any().and(post_filter());
 
